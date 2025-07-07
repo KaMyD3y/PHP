75025444a7d9d0875a7bd88bc52e67b3f394efb2
KaMyD3y <mihai.kamydzy@gmail.com>
2025-07-07 21:39:21 +0300

    PHP get numbers

---
+++ PHP/1.4.php
@@ -0,0 +1,42 @@
+<?php
+// PHP Taskbook Level 1.4
+// №1
+
+// Print to the console all integers from 1 to 100.
+for($i = 1;$i <= 100;$i++){
+    echo $i ."\n";
+}
+$array = 1;
+for(;$array <= 100;$array++){
+    echo $array ."\n";
+}
+foreach(range(1,100) as $i){
+    echo $i ."\n";
+}
+$i = 1;
+while($i <= 100){
+    echo $i ."\n";
+    $i++;
+}
+// №2
+
+// Print to the console all integers from -100 to 0.
+for($i = -100; $i <= 0; $i++){
+    echo $i ."\n";
+}
+foreach(range(-100,0) as $i){
+    echo $i ."\n";
+}
+// №3
+
+// Print to the console all integers from 100 to 1.
+
+// №4
+
+// Print to the console all even numbers from 1 to 100.
+
+// №5
+
+// Print to the console all numbers multiples of three in the range from 1 to 100.
+
+?>
